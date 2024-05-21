f1a81143868406cd4846e0279cae37b92d2a2206
Manuel <manuel.winkler@gmx.ch>
2024-05-21 23:29:38 +0200

    Working for ajax

--- conversion.cpp
+++ conversion.cpp
@@ -1,6 +1,7 @@
 #include <numeric>
 #include <raylib.h>
 #include <raymath.h>
+#include <cstdint>
 #include <rlgl.h>
 #include <cstring>
 #include <vector>
@@ -136,14 +137,15 @@ Rayhit Raycast(Ray r){
     Rayhit ret{};
     ret.tnear = hitex.ray.tnear;
     ret.tfar = hitex.ray.tfar;
+    
     if(it != gScene.geometry_mesh_map.end()){
         unsigned int faceID = hitex.hit.primID;
         Vector2 uvv1{it->second.first.texcoords[(faceID * 3 + 0) * 2], it->second.first.texcoords[(faceID * 3 + 0) * 2 + 1]};
         Vector2 uvv2{it->second.first.texcoords[(faceID * 3 + 1) * 2], it->second.first.texcoords[(faceID * 3 + 1) * 2 + 1]};
         Vector2 uvv3{it->second.first.texcoords[(faceID * 3 + 2) * 2], it->second.first.texcoords[(faceID * 3 + 2) * 2 + 1]};
-        Vector3 nrv1{it->second.first.normals  [(faceID * 3 + 0) * 3], it->second.first.normals  [(faceID * 3 + 0) * 3 + 1], it->second.first.normals [(faceID * 3 + 0) * 3 + 2]};
-        Vector3 nrv2{it->second.first.normals  [(faceID * 3 + 1) * 3], it->second.first.normals  [(faceID * 3 + 1) * 3 + 1], it->second.first.normals [(faceID * 3 + 1) * 3 + 2]};
-        Vector3 nrv3{it->second.first.normals  [(faceID * 3 + 2) * 3], it->second.first.normals  [(faceID * 3 + 2) * 3 + 1], it->second.first.normals [(faceID * 3 + 2) * 3 + 2]};
+        //Vector3 nrv1{it->second.first.normals  [(faceID * 3 + 0) * 3], it->second.first.normals  [(faceID * 3 + 0) * 3 + 1], it->second.first.normals [(faceID * 3 + 0) * 3 + 2]};
+        //Vector3 nrv2{it->second.first.normals  [(faceID * 3 + 1) * 3], it->second.first.normals  [(faceID * 3 + 1) * 3 + 1], it->second.first.normals [(faceID * 3 + 1) * 3 + 2]};
+        //Vector3 nrv3{it->second.first.normals  [(faceID * 3 + 2) * 3], it->second.first.normals  [(faceID * 3 + 2) * 3 + 1], it->second.first.normals [(faceID * 3 + 2) * 3 + 2]};
         Vector3 pv1{it->second.first.vertices  [(faceID * 3 + 0) * 3], it->second.first.vertices [(faceID * 3 + 0) * 3 + 1], it->second.first.vertices[(faceID * 3 + 0) * 3 + 2]};
         Vector3 pv2{it->second.first.vertices  [(faceID * 3 + 1) * 3], it->second.first.vertices [(faceID * 3 + 1) * 3 + 1], it->second.first.vertices[(faceID * 3 + 1) * 3 + 2]};
         Vector3 pv3{it->second.first.vertices  [(faceID * 3 + 2) * 3], it->second.first.vertices [(faceID * 3 + 2) * 3 + 1], it->second.first.vertices[(faceID * 3 + 2) * 3 + 2]};
@@ -151,16 +153,17 @@ Rayhit Raycast(Ray r){
         float y = hitex.hit.v;
         float z = 1.0f - x - y;
         Vector2 uv = Vector2Add(Vector2Scale(uvv1, x), Vector2Add(Vector2Scale(uvv2, y), Vector2Scale(uvv3, z)));
-        Vector3 nrm = Vector3Add(Vector3Scale(nrv1, x), Vector3Add(Vector3Scale(nrv2, y), Vector3Scale(nrv3, z)));
+        //Vector3 nrm = Vector3Add(Vector3Scale(nrv1, x), Vector3Add(Vector3Scale(nrv2, y), Vector3Scale(nrv3, z)));
         Vector3 pm = Vector3Add(Vector3Scale(pv1, x), Vector3Add(Vector3Scale(pv2, y), Vector3Scale(pv3, z)));
         if(it->second.second){
-            nrm = Vector3Transform_nt(nrm, it->second.second.value());
+            //nrm = Vector3Transform_nt(nrm, it->second.second.value());
             pm  = Vector3Transform   (pm , it->second.second.value());
         }
         ret.u = uv.x;
         ret.v = uv.y;
+        //ret.normal = nrm;
+        ret.normal = Vector3{hitex.hit.Ng_x, hitex.hit.Ng_y, hitex.hit.Ng_z};
         ret.position = pm;//Vector3Add(r.position, Vector3Scale(r.direction, hitex.ray.tfar));
-        ret.normal = nrm;
     }
     else{
         //abort();
@@ -172,11 +175,10 @@ int main(){
     
     gDevice = rtcNewDevice(NULL);
     gScene.m_scene = rtcNewScene(gDevice);
-    
-    InitWindow(1920, 1080, "");
-    SetTargetFPS(60);
+    rtcSetSceneBuildQuality(gScene.m_scene, RTC_BUILD_QUALITY_HIGH);
+    InitWindow(2560, 1440, "");
     Model cube = LoadModel("cube.obj");
-    Model human = LoadModel("human.obj");
+    Model human = LoadModel("ajax.obj");
     Model cottage = LoadModel("cottage.obj");
     rlDisableBackfaceCulling();
     //RTCGeometry geom = raylib_to_embree(mod.meshes[0]);
@@ -186,7 +188,14 @@ int main(){
         MatrixTranslate(0, 0, 0.2),
         MatrixTranslate(0, 0,   2),
     };
-    //reDrawMeshTransformed(human.meshes[0], MatrixTranslate(1.3765, -15.5177, 1.7395));
+    size_t meshcount = 300;
+    Matrix* instance_trfs = (Matrix*)std::aligned_alloc(64, meshcount * sizeof(Matrix));
+    for(size_t i = 0;i < meshcount;i++){
+        double arg = (double(i) / double(meshcount)) * 4 * M_PI;
+        instance_trfs[i] = MatrixMultiply(MatrixScale(0.01,0.01,0.01), MatrixTranslate(std::cos(arg) * std::sqrt(arg),-5 + arg/2.0,std::sin(arg) * std::sqrt(arg)));
+        //reDrawMeshTransformed(human.meshes[0], MatrixMultiply(MatrixScale(0.04,0.04,0.04), MatrixTranslate(std::cos(arg) * std::sqrt(arg),-5 + arg/2.0,std::sin(arg) * std::sqrt(arg))));
+    }
+    reDrawMeshInstanced(human.meshes[0], instance_trfs, meshcount);
     //for(int i = 0;i < cottage.meshCount;i++)
     //    reDrawMeshTransformed(cottage.meshes[i], MatrixTranslate(18.569975, -0.665788, 0.528871));
     //reDrawMeshTransformed(mod.meshes[0], MatrixTranslate(10, 10, 2));
@@ -194,44 +203,83 @@ int main(){
     //reDrawMeshTransformed(mod.meshes[0], MatrixTranslate(10, 0, 2));
     //auto vec = reDrawMeshInstanced(mod.meshes[0], trfs, 4);
     
-    reDrawMeshTransformed(cube.meshes[0], MatrixTranslate(0,0,0));
+    //reDrawMeshTransformed(cube.meshes[0], MatrixTranslate(0,0,0));
     //reDrawMeshTransformed(mod.meshes[0], trfs[0]);
     rtcCommitScene(gScene);
     Camera3D cam;
     cam.fovy = 45.0f;
     cam.projection = CAMERA_PERSPECTIVE;
-    cam.position = Vector3{-4,2,-4};
-    cam.target = Vector3{0,0,0};
-    cam.up = Vector3{0,1,0};
-    Image rtImage = GenImageColor(GetScreenWidth(), GetScreenHeight(), BLACK);
+    cam.position = Vector3{-10, 3, -10};
+    cam.target = Vector3{1e-3, 1e-3, 0};
+    cam.up = Vector3{0, 1, 0};
+    Image rtImage = GenImageColor(GetScreenWidth() / 2, GetScreenHeight(), BLACK);
     Texture tex = LoadTextureFromImage(rtImage);
-    
+    int w = GetScreenWidth();
+    int wh = GetScreenWidth() / 2;
+    int h = GetScreenHeight();
+    bool should_raytrace = true;
+    bool should_rasterize = true;
+    RenderTexture2D rtex = LoadRenderTexture(wh, h);
+    Material default_mat = LoadMaterialDefault();
+    int raytrace_supersampling = 1;
     while(!WindowShouldClose()){
+        
         ClearBackground(Color { 50, 50, 70, 255 });
-        BeginMode3D(cam);
-        DrawMesh(cube.meshes[0], LoadMaterialDefault(), MatrixTranslate(0,0,0));
-        EndMode3D();
-        DrawText(TextFormat("FPS: %d", GetFPS()), 0, 0, 50, GREEN);
+        
         Ray r{.position  = Vector3{0, 0.0, 1000},
               .direction = Vector3{0.0, 0.0, -1}
             };
-        RTCRayHit hitex = RaycastEx(r);
-        DrawText(TextFormat("hitex.ray.tfar  : %f", hitex.ray.tfar  ), 0, 100, 40, YELLOW);
-        DrawText(TextFormat("hitex.hit.geomID: %d", hitex.hit.geomID), 0, 150, 40, YELLOW);
-        DrawText(TextFormat("hitex.hit.instID[0]: %d", hitex.hit.instID[0]), 0, 200, 40, YELLOW);
-        DrawText(TextFormat("hitex.hit.primID: %d", hitex.hit.primID), 0, 250, 40, YELLOW);
-        DrawText(TextFormat("hitex.hit.instPrimID[0]: %d", hitex.hit.instPrimID[0]), 0, 300, 40, YELLOW);
-        Rayhit hit = Raycast(r);
-        DrawText(TextFormat("hit.tfar: %f", hit.tfar), 600, 100, 40, YELLOW);
-        DrawText(TextFormat("hit.uv   : %f, %f", hit.u, hit.v), 600, 150, 40, YELLOW);
-        DrawText(TextFormat("hit.pos   : (%f, %f, %f)", hit.position.x, hit.position.y, hit.position.z), 600, 200, 40, YELLOW);
-        DrawText(TextFormat("hit.posacc: (%f, %f, %f)", hit.position.x, hit.position.y, hit.position.z), 600, 250, 40, YELLOW);
-        DrawText(TextFormat("hit.normal: (%f, %f, %f)", hit.normal.x, hit.normal.y, hit.normal.z), 600, 300, 40, YELLOW);
-        //DrawText(TextFormat("hit.hit.instID[0]: %d", hitex.hit.instID[0]),        600, 200, 40, YELLOW);
-        //DrawText(TextFormat("hit.hit.primID: %d", hitex.hit.primID),              600, 250, 40, YELLOW);
-        //DrawText(TextFormat("hit.hit.instPrimID[0]: %d", hitex.hit.instPrimID[0]),600, 300, 40, YELLOW);
-        UpdateTexture(tex, rtImage.data);
-        DrawTexture(tex,0,0,WHITE);
+        if(should_raytrace){
+            #pragma omp parallel for collapse(2) schedule(guided)
+            for(int i = 0;i < h;i++){
+                for(int j = 0;j < wh;j++){
+                    Vector3 normalized_rgb{0,0,0};
+                    for(int ssi = 0;ssi < raytrace_supersampling;ssi++){
+                        for(int ssj = 0;ssj < raytrace_supersampling;ssj++){
+                            Ray r = GetScreenToWorldRayEx(Vector2{float(j) + (float(ssj) / raytrace_supersampling), float(i) + (float(ssi) / raytrace_supersampling)}, cam, wh, h);
+                            auto hit = Raycast(r);
+                            if(!std::isinf(hit.tfar)){
+                                normalized_rgb.x += -hit.normal.y * (1.0f / raytrace_supersampling / raytrace_supersampling);
+                                normalized_rgb.y += -hit.normal.y * (1.0f / raytrace_supersampling / raytrace_supersampling);
+                                normalized_rgb.z += -hit.normal.y * (1.0f / raytrace_supersampling / raytrace_supersampling);
+                            }
+                        }
+                    }
+                    ((Color*)rtImage.data)[(i * wh + j)] = Color{
+                        (unsigned char)(std::min(normalized_rgb.x, 1.0f) * 255),
+                        (unsigned char)(std::min(normalized_rgb.y, 1.0f) * 255),
+                        (unsigned char)(std::min(normalized_rgb.z, 1.0f) * 255), 
+                    255};
+                    
+                }
+            }
+            UpdateTexture(tex, rtImage.data);
+            DrawTexture(tex,0,0,WHITE);
+        }
+        DrawText(TextFormat("FPS: %d", GetFPS()), 0, 0, 20, GREEN);
+        DrawText("Raytraced [Toggle R]", wh / 2 - 100, 0, 20, should_raytrace ? GREEN : RED);
+        DrawText("Default [Toggle D]", wh * 3 / 2 - 100, 0, 20, should_rasterize ? GREEN : RED);
+        if(IsKeyPressed(KEY_D)){
+            should_rasterize = !should_rasterize;
+        }
+        if(IsKeyPressed(KEY_R)){
+            should_raytrace = !should_raytrace;
+        }
+        if(should_rasterize){
+            BeginTextureMode(rtex);
+            ClearBackground(BLACK);
+            BeginMode3D(cam);
+            for(size_t i = 0;i < meshcount;i++){
+                DrawMesh(human.meshes[0], default_mat, instance_trfs[i]);
+            }
+            //DrawMeshInstanced(human.meshes[0], default_mat, instance_trfs, meshcount);
+            EndMode3D();
+            EndTextureMode();
+            DrawTexturePro(rtex.texture,Rectangle{0,0,(float)wh, (float)-h}, Rectangle{(float)wh, 0, (float)wh, (float)h}, Vector2{0,0},0.0f,WHITE);
+        }
+        rlSetLineWidth(5.0f);
+        DrawLine(wh,0,wh,h,BLACK);
+
         EndDrawing();
     }
 }
